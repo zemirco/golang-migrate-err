72251e84f24fd980445559c0b270f696d766ad10
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-05-21 11:27:13 +0200

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zemirco/golang-migrate-err
+
+go 1.12
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.3.1
+	github.com/lib/pq v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	// use with local kubernetes cluster on minikube
+	source := "user=postgres host=192.168.99.100 port=32283 dbname=postgres sslmode=disable"
+	// use with Circle CI
+	// source := "user=postgres host=localhost dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", source)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		panic(err)
+	}
+	migration, err := migrate.NewWithDatabaseInstance("file://migrations", "postgres", driver)
+	if err != nil {
+		panic(err)
+	}
+	if err := migration.Drop(); err != nil {
+		panic(err)
+	}
+	if err := migration.Up(); err != nil {
+		panic(err)
+	}
+}
